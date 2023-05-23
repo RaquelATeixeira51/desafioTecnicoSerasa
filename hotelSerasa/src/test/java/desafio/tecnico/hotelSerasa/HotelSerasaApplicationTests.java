10221543b9004beb033d8599dd0aaa85d457e478
RaquelATeixeira <85255156+RaquelATeixeira@users.noreply.github.com>
2023-05-23 12:49:06 -0300

    adding maven project structure

---
+++ hotelSerasa/src/test/java/desafio/tecnico/hotelSerasa/HotelSerasaApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.tecnico.hotelSerasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSerasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
